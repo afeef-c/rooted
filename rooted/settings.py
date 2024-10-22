19c876460b87cee61cb463b9a66da4a04df245cc
afeef-c <afeefc123@gmail.com>
2024-10-22 13:22:50 +0530

    initial commit.1.1

--- rooted/settings.py
+++ rooted/settings.py
@@ -164,8 +164,8 @@ AWS_S3_VERIFY = os.getenv('AWS_S3_VERIFY') == 'True'
 
 
 # Media files (stored in S3)
-# MEDIA_URL = f'https://{os.getenv("AWS_STORAGE_BUCKET_NAME")}.s3.amazonaws.com/media/'
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{os.getenv("AWS_STORAGE_BUCKET_NAME")}.s3.amazonaws.com/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Disable InsecureRequestWarning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
